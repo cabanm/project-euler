5939e3ad1e6b5f3905e5f03d16366d082298fb49
Miro Caban <miro.caban@gmail.com>
2014-08-21 01:04:11 +0200

    solved problem54

--- problem54/problem54.py
+++ problem54/problem54.py
@@ -1,64 +1,70 @@
 class hand:
     def __init__(self, string):
+        self.string = string
         self.values = None
         self.sets = None
-        values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        values = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
         suits = ['H','D','S','C']
         self.values = [string.count(x) for x in values]
         self.suits = [string.count(x) for x in suits]
 
-def highCard(hand):
-    for i,x in enumerate(hand):
-        
+def nthIndex(n, x, List):
+    if n==-1: return len(List)-List[::-1].index(x)-1
+    last = 0
+    for i in range(n):
+        last = List.index(x, last) + 1
+    return last-1
 
 def onepair(hand):
-    return 2 in hand.values
+    if 2 in hand.values:
+        return [1, hand.values.index(2)]
+    return []
 
 def twopair(hand):
-    return hand.values.count(2) == 2
+    if hand.values.count(2) == 2:
+        return [2, nthIndex(2,2,hand.values), nthIndex(1,2,hand.values)]
+    return []
 
 def triple(hand):
-    return 3 in hand.values
+    if 3 in hand.values:
+        return [3, hand.values.index(3)]
+    return []
 
 def straight(hand):
     for i in range(len(hand.values)-4):
-        if hand.values[i:i+5] == [1]*5: return True
-    return False
+        if hand.values[i:i+5] == [1]*5:
+            return [4, nthIndex(-1,1,hand.values)]
+    return []
 
 def flush(hand):
-    if 5 in hand.suits: return (hand.suits.index(5))
+    if 5 in hand.suits: return [5, nthIndex(-1,1,hand.values)]
+    return []
 
 def fullhouse(hand):
-    return onepair(hand) and triple(hand)
+    if onepair(hand)!=[] and triple(hand)!=[]:
+        return [6, hand.values.index(3), hand.values.index(2)]
+    return []
 
 def quad(hand):
-    return 4 in hand.values
+    if 4 in hand.values: return [7, hand.values.index(4)]
+    return []
 
 def str8flush(hand):
-    return straight(hand) and flush(hand)
+    if straight(hand)!=[] and flush(hand)!=[]:
+        return [8, nthIndex(-1,1,hand.values)]
+    return []
 
 def royalflush(hand):
-    return flush(hand) and hand.values[-5:] == [1]*5
-
-def compareHighCards(hand1, hand2):
-    high1 = high2 = 0
-    if hand1.values.pop(-1) != 0: high1 = 1
-    if hand2.values.pop(-1) != 0: high2 = 1
-    if high1 > high2: return 0
-    if high1 < high2: return 1
-    compareHighCards(hand1, hand2)
+    if flush(hand) and hand.values[-5:] == [1]*5: return [9]
+    return []
 
 def rank(hand):
-    if royalflush(hand): return 9
-    if str8flush(hand): return 8
-    if quad(hand): return 7
-    if fullhouse(hand): return 6
-    if flush(hand): return 5
-    if straight(hand): return 4
-    if triple(hand): return 3
-    if twopair(hand): return 2
-    if onepair(hand): return 1
-    return 0
+    plays = [royalflush, str8flush, quad, fullhouse, flush, straight, triple, twopair, onepair]
+    for fn in plays:
+        score = fn(hand)
+        if len(score)!=0: break
+    if len(score)==0: score=[0]
+    return score + [1 if x>0 else 0 for x in hand.values[::-1]]
 
 f = open('poker.txt')
 
@@ -70,17 +76,15 @@ for line in f:
     hand2 = hand(' '.join(line[5:]))
     rank1 = rank(hand1)
     rank2 = rank(hand2)
-    print rank1, rank2
-    if rank1 > rank2:
-        count1 += 1
-        continue
-    if rank1 < rank2:
-        count2 += 1
-        continue
-    if compareHighCards(hand1, hand2) == 0:
-        count1 += 1
-    else:
-        count2 += 1
+    print rank1
+    print rank2
+    for i in range(len(rank1)):
+        if rank1[i]>rank2[i]:
+            count1 += 1
+            break
+        elif rank2[i]>rank1[i]:
+            count2 += 1
+            break
     print count1, count2
 
 f.close()
