5b222b9cb01eb479f79774469360217c22992814
Miro Caban <miro.caban@gmail.com>
2014-08-21 00:59:06 +0200

    solved problem59

--- problem59/problem59.py
+++ problem59/problem59.py
@@ -1,13 +1,28 @@
-def xor(m,n):
-	out = '0b'
-	m = bin(m)[2:]
-	n = bin(n)[2:]
-	lenm = len(m)
-	lenn = len(n)
-	if lenm > lenn:
-		for i in range(lenm-lenn):
-			n += n[i]
-	if lenn > lenm:
-		for i in range(lenn-lenm):
-			m += '0'+m
-	return int('0b'+''.join([str(int(i!=j)) for i,j in zip(m,n)]),2)
+def decipher(ciphered_list, key):
+    assert len(key) == 3, 'Key size must be 3'
+    key_list = [ord(c) for c in key]
+    text = ''
+    for i in range(len(ciphered_list)):
+        text += chr(key_list[i%3] ^ ciphered_list[i])
+    return text
+
+f = open('p059_cipher.txt')
+
+ciphered_list = f.readline().strip().split(',')
+ciphered_list = [int(s) for s in ciphered_list]
+N = len(ciphered_list)
+alphabet = range(97,123)
+
+
+for l in alphabet:
+    for m in alphabet:
+        for n in alphabet:
+            key = chr(l) + chr(m) + chr(n)
+            text = decipher(ciphered_list, key)
+            count = sum([text.count(s) for s in ['and','And','aNd','anD','ANd','AnD','aND','AND']])
+            if count > 2:
+                print key, count
+                print text
+                print 'sum:', sum([ord(c) for c in text]), '\n'
+
+f.close()
